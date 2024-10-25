391ec849858ffe357f9c2dc3ce16e65f20ee5e90
Sanskar Suryavanshi <sanskarsuryavanshi@Sanskars-MacBook-Air.local>
2024-10-26 02:30:33 +0530

    added individual prod detail

--- app/_components/ProductItem.jsx
+++ app/_components/ProductItem.jsx
@@ -1,31 +1,50 @@
 import { Button } from '@/components/ui/button';
-import Image from 'next/image'
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import ProductItemDetail from './ProductItemDetail';
 
-function ProductItem({product, index}) {
-    if (!product.images || product.images.length === 0) {
-        console.log("this product is doesn't have image",product)
-        return null; // Skip this product if images are not available
-      }
+function ProductItem({ product, index }) {
+  if (!product.images || product.images.length === 0) {
+    console.log("This product doesn't have an image", product);
+    return null; // Skip this product if images are not available
+  }
 
-      return (
-        <div className='flex flex-col items-center gap-3 bg-green-100 p-3 rounded-lg shadow-lg cursor-pointer
-        hover:scale-110 hover:shadow-md transition-all ease-in-out
-        ' key={index}>
-          <Image
-            className='h-[200px] w-[200px] rounded-2xl'
-            src={`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}${product.images[0].url}`}
-            alt='icon'
-            width={500}
-            height={200}
-          />
-          <h2 className='text-green-800 text-sm '>{product.name}</h2>
-          <h3 className='flex justify-center w-full align-bottom'>${product.price}</h3>
-          <Button className='px-5 py-3'>Add to cart</Button>
-          {/* <Button className=' md:px- py-3'>Add to cart</Button> */}
+  return (
+    <div className='flex flex-col items-center gap-3 bg-green-100 p-3 rounded-lg shadow-lg cursor-pointer
+      hover:scale-110 hover:shadow-md transition-all ease-in-out'
+      key={index}>
+      <Image
+        className='h-[200px] w-[200px] md:h-[250px] md:w-[250px] rounded-2xl object-cover'
+        src={`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}${product.images[0].url}`}
+        alt='icon'
+        width={500}
+        height={200}
+      />
+      <h2 className='text-green-800 text-sm md:text-base'>{product.name}</h2>
 
-        </div>
-      );
+     
+      <h3 className='flex justify-center w-full align-bottom text-sm md:text-lg'>${product.price}</h3>
+      <Dialog>
+        <DialogTrigger>
+          <Button className='px-4 py-2 md:px-5 md:py-3'>Add to cart</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogDescription>
+              <ProductItemDetail product={product} />
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
 }
 
-export default ProductItem
+export default ProductItem;
---
+++ app/_components/ProductItemDetail.jsx
@@ -0,0 +1,46 @@
+import Image from 'next/image';
+import React from 'react';
+import { Button } from '@/components/ui/button';
+import { ShoppingBag } from 'lucide-react';
+
+function ProductItemDetail({ product }) {
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
+      <Image
+        src={`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}${product.images[0].url}`}
+        alt='image'
+        width={300}
+        height={300}
+        className='rounded-2xl object-contain'
+      />
+      <div className='flex flex-col gap-5'>
+        <div>
+
+        <h2 className='text-xl md:text-2xl text-primary'>{product.name}</h2>
+        {product.categories != null && <h3 className='text-sm'>{product.categories.name}</h3>}
+        </div>
+        <h2 className='text-sm md:text-base'>{product.description}</h2>
+        <div className='flex gap-2 mt-4'>
+          <h2 className='text-sm font-bold'>Soil Type :</h2>
+          <h2 className='text-sm'>{product.soilType}</h2>
+        </div>
+        <div className='flex gap-2 mt-3 align-middle'>
+          <h2 className='text-2xl'>${product.price}</h2>
+        </div>
+        <div className='flex gap-2 items-center'>
+          <h2 className='font-bold'>Quantity</h2>
+          <div className='flex gap-2'>
+            <Button>-</Button>
+            <Button>1</Button>
+            <Button>+</Button>
+          </div>
+        </div>
+        <div className='mt-3 flex justify-end'>
+          <Button>Add to Cart <ShoppingBag /></Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductItemDetail;
