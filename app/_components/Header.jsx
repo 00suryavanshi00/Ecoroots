9481cfa7665b272f8cf492926f04247d91f6ef3a
Sanskar Suryavanshi <sanskarsuryavanshi@Sanskars-MacBook-Air.local>
2024-10-28 23:12:53 +0530

    gave bucket public access

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -13,6 +13,7 @@ import { LayoutGrid, Search, ShoppingBag } from 'lucide-react'
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 import { getCategory } from '../_utils/GlobalApi'
+import Link from 'next/link'
 
 function Header() {
 
@@ -32,7 +33,7 @@ function Header() {
     <div className='p-4 shadow-sm flex justify-between'>
         <div className='flex items-center gap-10 '>
 
-        <Image src='https://storage.googleapis.com/ecoroots_assets_bucket/Eco_Roots_Sans_deb523bbe8/Eco_Roots_Sans_deb523bbe8.svg?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=590516909829-compute%40developer.gserviceaccount.com%2F20241028%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20241028T164433Z&X-Goog-Expires=900&X-Goog-SignedHeaders=host&X-Goog-Signature=3057bfdb5983173f6b54279714c515bd9b8d8e2be93855363d5635770b1f43895ef23f3231892519a51b490a6877f1a52eac4a7e3fcb1b0d296113844a32d47e78f5dbe210adf0d7f417218867749469a2f8d7599b0cc9b5d69561137cba32b52c9f6de4a57ddbe3a734cad9761320f303eecd9f0979457d8e16c119ede379627f4964441180a49a9b964c8a64687a75fb3871fe28a34de79a52ed35da208fadb5af529ce78ee2ee9c81f6ca36c8eb7f0b3150a533f55336939d11b62535256a8bd940ca887eea1aedf8fd50a154c8a9ec6b672b0e17a05c303292afbf6a1a635f8b1ab9ab9aaa7cd389f965c83994daf27c689b5f542306ed81b647fe8ea3f8' alt='logo' width={150} height={150}/>
+        <Image src='https://storage.googleapis.com/ecoroots_assets_bucket/Eco_Roots_Sans_deb523bbe8/Eco_Roots_Sans_deb523bbe8.svg?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=590516909829-compute%40developer.gserviceaccount.com%2F20241028%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20241028T173847Z&X-Goog-Expires=900&X-Goog-SignedHeaders=host&X-Goog-Signature=684635be0ac45639834cc00cea49114f689d4a4a6f0449e86a4adc4ef433863982ce848eb04721bb15d05449257ffa25518710742be18dfdb375dc71ea15e0a9a7617ce905a3c65a874515117c8ee085962031f8a26258ac35b127968f81b2c0ed3a2a90812ba099f6a364d29c920a973ea4aea0657f3c550a18400e0dc9c2bed9d95c3e7b5471b700002fdd0cda760a720bbf61f62463dd9dacc5172b02140e22690786853e48eaa0fd9e994dde890fb5597a816795e1045dc285af3f0e374e5d1a40d932bfe5484530be04809c1be7547dd3b63998513c133676ec26251ac096ba7dc93dd2b189f1775e5fe031125b4516c2e5b85481c0d38a303e6e59e214' alt='logo' width={150} height={150}/>
 
             <DropdownMenu>
             <DropdownMenuTrigger className='outline-none'>
@@ -47,10 +48,11 @@ function Header() {
     {categorylist.map((category, index) => {
 
     return (
+        <Link href={'/products-categories/' + category.name}>
         <DropdownMenuItem className='flex gap-4 items-center p-2' key={index}>
             <Image src={`${category?.icon[0].url}`} alt='logo' width={30} height={30}/>
             <h2 className='text-primary text-lg'>{category.name}</h2>
-        </DropdownMenuItem>
+        </DropdownMenuItem></Link>
     );
 })}
     {/* <DropdownMenuItem className='text-primary'>Flowering Plants</DropdownMenuItem>
